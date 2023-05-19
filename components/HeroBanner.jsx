83220b2cd8e52bad73b4693fbc4b16e80ac04f4c
muhsin-web <muhsinabdulrasheed@gmail.com>
2023-05-19 15:18:16 +0100

    ...

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -5,11 +5,11 @@ import HeroImg from '../public/image 2.png'
 const HeroBanner = () => {
   return (
     <Flex height='90vh' justifyContent='space-between' alignItems='center'>
-          <Box color='#fff' paddingLeft='60px'>
+          <Box fontWeight='400' color='#fff' paddingLeft='60px'>
                <Text mb='10px' fontSize='16px' color='#FBB034'>Welcome To Barbera</Text>
-               <Heading lineHeight='46px' fontSize='48px'>We Will Give You An Impeccable Look</Heading>
+               <Heading fontWeight='700' lineHeight='46px' fontSize='48px'>We Will Give You An Impeccable Look</Heading>
                <Text m='10px 0' lineHeight='22px' fontSize='21px' color='#9A9A9A'>You will leave lookin sharp, relaxed and ready to take on the world, with that swagger in your stride.</Text>
-               <Button mt='20px' fontSize='15px' bg='#FBB034'>Book An Appointment</Button>
+               <Button fontWeight='700' mt='20px' color='#202125' padding='5px 20px' fontSize='15px' bg='#FBB034'>Book An Appointment</Button>
           </Box>
 
           <Box height='100%' position='relative' width='1000px' backgroundImage="url(`${HeroImg}`)">
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,14 +1,33 @@
-import { Flex, Box, Text, Stack, Link } from '@chakra-ui/react'
+import { Flex, Box, Text, HStack, Link } from '@chakra-ui/react'
 import React from 'react'
 import Image from 'next/image'
 import Logo from '../public/logo.png'
+import {BsFacebook, BsTwitter, BsLinkedin, BsInstagram} from 'react-icons/bs'
+
 
 const Navbar = () => {
   return (
     <Box>
           <Flex justifyContent='flex-end' p='10px 60px' bg='#FBB034'>
                <Text>Opening Hour :  Sunday - Friday , 08:00 am - 10:00 pm</Text>
-               <Stack>icons</Stack>
+               
+               <HStack color='#FBB034' marginLeft='130px'>
+                    <Link marginRight='0px' padding='5px' background='#202125' borderRadius='50%'>
+                         <BsFacebook />
+                    </Link>
+
+                    <Link marginRight='10px' padding='5px' background='#202125' borderRadius='50%'>
+                    <BsTwitter />
+                    </Link>
+
+                    <Link marginRight='40px' padding='5px' background='#202125' borderRadius='50%'>
+                    <BsLinkedin />
+                    </Link>
+
+                    <Link marginRight='20px' padding='5px' background='#202125' borderRadius='50%'>
+                    <BsInstagram />
+                    </Link>
+               </HStack>
           </Flex>
           <Flex justifyContent='space-between' bg='#11120F' p='10px 60px'>
                <Box boxSize='50px'>
@@ -17,7 +36,7 @@ const Navbar = () => {
                <Box>
                     {
                          ['Home', 'About Us', 'services', 'Contact', 'Get the App'].map((link, index) => (
-                              <Link key={index} color='#fff' marginRight='20px'>{link}</Link>
+                              <Link _hover={{color: '#FBB034', transition: 'all .3s ease'}} key={index} color='#fff' marginRight='20px'>{link}</Link>
                          ))
                     }
                </Box>
